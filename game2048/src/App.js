554ebad64bcc879edc7e38f6630915d2d5a51223
Daria Lazovskaya <32599716+odasco@users.noreply.github.com>
2019-01-03 16:56:33 +0300

    field+static playground

--- game2048/src/App.js
+++ game2048/src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Layout from 'UI/Layout'
+import Field from 'UI/Field'
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Layout>
+          <Field cells={[
+              {id:  0, y: 0, x: 0, value:     2},
+              {id:  1, y: 0, x: 1, value:     4},
+              {id:  2, y: 0, x: 2, value:     8},
+              {id:  3, y: 0, x: 3, value:    16},
+              {id:  4, y: 1, x: 0, value:    32},
+              {id:  5, y: 1, x: 1, value:    64},
+              {id:  6, y: 1, x: 2, value:   128},
+              {id:  7, y: 1, x: 3, value:   256},
+              {id:  8, y: 2, x: 0, value:   512},
+              {id:  9, y: 2, x: 1, value:  1024},
+              {id: 10, y: 2, x: 2, value:  2048},
+              {id: 11, y: 2, x: 3, value:  4096},
+              {id: 12, y: 3, x: 0, value:  8192},
+              {id: 13, y: 3, x: 1, value: 16384},
+              {id: 14, y: 3, x: 2, value: 32768},
+              {id: 15, y: 3, x: 3, value: 65536},
+          ]} />
+      </Layout>
+    )
   }
 }
 
-export default App;
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+export default App
